aa51e41f9e62e7f1a9c8a6e71208ec84dc174524
sunilnjc <51849589+sunilnjc@users.noreply.github.com>
2024-08-19 07:30:22 +0400

    Added IpHash algorithm
    Added IpHash algorithm

--- src/RoundRobin.ts
+++ src/RoundRobin.ts
@@ -25,15 +25,43 @@ class RoundRobin {
     return server;
   }
 
+  /*
+   * Implementing the ip hash logic in here
+   * @param ip
+   * @returns the server based on the hash of the ip
+   */
+  private ipHash(ip: string): string {
+    const hash = require("crypto").createHash("md5").update(ip).digest("hex");
+    const index = parseInt(hash.substring(0, 8), 16) % this.servers.length;
+    return this.servers[index];
+  }
+
   /**
    * Proxying requests to the next target server
    * @param port
    */
-  public start(port: number) {
+  public start(port: number, algo: string) {
     const app = express();
 
     app.all("*", (req: Request, res: Response) => {
-      const target = this.getNextServer();
+      let target: string;
+
+      switch (algo) {
+        case "iphash":
+          const clientIp = req.ip || "";
+          target = this.ipHash(clientIp);
+          console.log("clientIp :: ", clientIp);
+          console.log(`iPhash Proxying request to : ${target}`);
+          break;
+        case "roundrobin":
+          target = this.getNextServer();
+          console.log(`Round Robin Proxying request to : ${target}`);
+          break;
+        default:
+          target = this.getNextServer();
+          console.log(`Proxying request to : ${target}`);
+      }
+
       console.log(`Proxying request to : ${target}`);
       this.proxy.web(req, res, { target });
     });
@@ -52,4 +80,4 @@ const servers = [
 ];
 
 const loadBalancer = new RoundRobin(servers);
-loadBalancer.start(3000);
+loadBalancer.start(3000, "iphash");
