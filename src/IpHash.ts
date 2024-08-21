93ae36e3189da196e9ee9decd47e238ed7a8fb51
sunilnjc <51849589+sunilnjc@users.noreply.github.com>
2024-08-21 07:58:38 +0400

    Added IpHash algorithm
    Added IpHash algorithm

--- src/IpHash.ts
+++ src/IpHash.ts
@@ -1,7 +1,8 @@
 import express, { Request, Response } from "express";
 import httpProxy from "http-proxy";
+import { server } from "typescript";
 
-class RoundRobin {
+class IpHash {
   private servers: string[];
   private currentServerIndex: number;
   private proxy: httpProxy;
@@ -12,28 +13,29 @@ class RoundRobin {
     this.proxy = httpProxy.createProxyServer({});
   }
 
-  /**
-   * Implementing the round robin logic in here
-   * @returns the next server in the list
+  /*
+   * Implementing the ip hash logic in here
+   * @param ip
+   * @returns the server based on the hash of the ip
    */
-  private getNextServer(): string {
-    const server = this.servers[this.currentServerIndex];
-    this.currentServerIndex =
-      (this.currentServerIndex + 1) % this.servers.length;
-
-    return server;
+  private ipHash(ip: string): string {
+    const hash = require("crypto").createHash("md5").update(ip).digest("hex");
+    const index = parseInt(hash.substring(0, 8), 16) % this.servers.length;
+    return this.servers[index];
   }
 
   /**
    * Proxying requests to the next target server
    * @param port
    */
-  public start(port: number) {
+  public start(port: number, algo: string) {
     const app = express();
 
     app.all("*", (req: Request, res: Response) => {
-      const target = this.getNextServer();
-      console.log(`Round Robin Proxying request to : ${target}`);
+      const clientIp = req.ip || "";
+      const target = this.ipHash(clientIp);
+      console.log("clientIp :: ", clientIp);
+      console.log(`iPhash Proxying request to : ${target}`);
 
       console.log(`Proxying request to : ${target}`);
       this.proxy.web(req, res, { target });
@@ -52,5 +54,5 @@ const servers = [
   "http://localhost:3004",
 ];
 
-const loadBalancer = new RoundRobin(servers);
-loadBalancer.start(3000);
+const loadBalancer = new IpHash(servers);
+loadBalancer.start(3000, "iphash");
